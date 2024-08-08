699a28eff14626c6a2abed69ee86a15f8325cc19
XiYang6666 <1782356858@qq.com>
2024-08-08 18:23:05 +0800

    :art: format

--- src/main/kotlin/xyz/xasmc/hashbook/util/HashUtil.kt
+++ src/main/kotlin/xyz/xasmc/hashbook/util/HashUtil.kt
@@ -3,7 +3,7 @@ package xyz.xasmc.hashbook.util
 import java.security.MessageDigest
 
 object HashUtil {
-    val md = MessageDigest.getInstance("SHA-256")
+    private val md = MessageDigest.getInstance("SHA-256")
 
     @OptIn(ExperimentalStdlibApi::class)
     fun hashString(str: String): String {
